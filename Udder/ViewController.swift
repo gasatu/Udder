c106a7678ef7357d2df8da54dd6cbe8e4b625a15
Gasatu <sousou.1.23.moutoku.west@gmail.com>
2020-08-13 11:32:05 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by HIRO on 2020/08/13.
+//  Copyright © 2020 HIRO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
